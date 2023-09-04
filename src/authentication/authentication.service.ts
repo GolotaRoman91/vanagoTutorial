df64ac494236aef6fe648345987256b4ddd24423
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-09-05 00:40:31 +0300

    Add getAuthenticatedUser method

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -30,4 +30,24 @@ export class AuthenticationService {
       );
     }
   }
+
+  public async getAuthenticatedUser(email: string, hashedPassword: string) {
+    try {
+      const user = await this.usersService.getByEmail(email);
+      const isPasswordMatching = await bcrypt.compare(
+        hashedPassword,
+        user.password,
+      );
+      if (!isPasswordMatching) {
+        throw new HttpException(
+          'Wroong credentials provided',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      user.password = undefined;
+      return user;
+    } catch (error) {
+      throw new HttpException('Wrong credentials', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
