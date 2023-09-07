d03927a10179c732fd77463e49645f73c7fb45d7
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-09-07 21:20:01 +0300

    Add register method

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import RegisterDto from './dto/register.dto';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authenticationService: AuthenticationService) {}
+
+  @Post('register')
+  async register(@Body() registrationData: RegisterDto) {
+    return this.authenticationService.register(registrationData);
+  }
+}
